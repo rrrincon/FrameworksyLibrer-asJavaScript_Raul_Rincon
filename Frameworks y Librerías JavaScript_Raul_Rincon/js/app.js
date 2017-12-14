59913d06485275998d9db74acafc68bfd9bbd047
rrrincon <34554096+rrrincon@users.noreply.github.com>
2017-12-14 17:44:33 -0500

    Add files via upload

---
+++ Frameworks y Librerías JavaScript_Raul_Rincon/js/app.js
@@ -0,0 +1,384 @@
+//Cambio de Color para Match Game
+function coloresTitulo(selector) {
+  $(selector).delay(1000).animate({
+    opacity: '0,5'
+  },
+  {
+    step: function() {
+      $(this).css('color', '#BB8FCE');
+    },
+    queue: true
+  }
+  )
+  .delay(1000)
+  .animate({
+    opacity: '1'
+  },
+  {
+    step: function() {
+      $(this).css('color', 'yellow')
+      coloresTitulo('h1.main-titulo');
+    },
+    queue: true
+  }
+  );
+}
+
+//Generar Numeros Aleatoriamente
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//Para las imagenes en las columnas
+function giveCandyArrays(arrayType, index) {
+
+  var candyCol1 = $('.col-1').children();
+  var candyCol2 = $('.col-2').children();
+  var candyCol3 = $('.col-3').children();
+  var candyCol4 = $('.col-4').children();
+  var candyCol5 = $('.col-5').children();
+  var candyCol6 = $('.col-6').children();
+  var candyCol7 = $('.col-7').children();
+
+  var candyColumns = $([candyCol1, candyCol2, candyCol3, candyCol4,
+                       candyCol5, candyCol6, candyCol7]);
+
+  if (typeof index === 'number') {
+    var candyRow = $([candyCol1.eq(index), candyCol2.eq(index), candyCol3.eq(index),
+                      candyCol4.eq(index), candyCol5.eq(index), candyCol6.eq(index),
+                      candyCol7.eq(index)]);
+  } else {
+    index = '';
+  }
+
+  if (arrayType === 'columns') {
+    return candyColumns;
+  } else if (arrayType === 'rows' && index !== '') {
+    return candyRow;
+  }
+}
+
+//Para las Filas
+function candyRows(index) {
+  var candyRow = giveCandyArrays('rows', index);
+  return candyRow;
+}
+
+//Para las columnas
+function candyColumns(index) {
+  var candyColumn = giveCandyArrays('columns');
+  return candyColumn[index];
+}
+
+//Que imagenes iguales coincidan en las columnas
+function columnValidation() {
+  for (var j = 0; j < 7; j++) {
+    var counter = 0;
+    var candyPosition = [];
+    var extraCandyPosition = [];
+    var candyColumn = candyColumns(j);
+    var comparisonValue = candyColumn.eq(0);
+    var gap = false;
+    for (var i = 1; i < candyColumn.length; i++) {
+      var srcComparison = comparisonValue.attr('src');
+      var srcCandy = candyColumn.eq(i).attr('src');
+
+      if (srcComparison != srcCandy) {
+        if (candyPosition.length >= 3) {
+          gap = true;
+        } else {
+          candyPosition = [];
+        }
+        counter = 0;
+      } else {
+        if (counter == 0) {
+          if (!gap) {
+            candyPosition.push(i-1);
+          } else {
+            extraCandyPosition.push(i-1);
+          }
+        }
+        if (!gap) {
+          candyPosition.push(i);
+        } else {
+          extraCandyPosition.push(i);
+        }
+        counter += 1;
+      }
+      comparisonValue = candyColumn.eq(i);
+    }
+    if (extraCandyPosition.length > 2) {
+      candyPosition = $.merge(candyPosition, extraCandyPosition);
+    }
+    if (candyPosition.length <= 2) {
+      candyPosition = [];
+    }
+    candyCount = candyPosition.length;
+    if (candyCount >= 3) {
+      deleteColumnCandy(candyPosition, candyColumn);
+      setScore(candyCount);
+    }
+  }
+}
+
+//Cambiar el tipo de imagen
+function deleteColumnCandy(candyPosition, candyColumn) {
+  for (var i = 0; i < candyPosition.length; i++) {
+    candyColumn.eq(candyPosition[i]).addClass('delete');
+  }
+}
+
+//Para que imagenes iguales coincidan en las filas
+function rowValidation() {
+  for (var j = 0; j < 6; j++) {
+    var counter = 0;
+    var candyPosition = [];
+    var extraCandyPosition = [];
+    var candyRow = candyRows(j);
+    var comparisonValue = candyRow[0];
+    var gap = false;
+    for (var i = 1; i < candyRow.length; i++) {
+      var srcComparison = comparisonValue.attr('src');
+      var srcCandy = candyRow[i].attr('src');
+
+      if (srcComparison != srcCandy) {
+        if (candyPosition.length >= 3) {
+          gap = true;
+        } else {
+          candyPosition = [];
+        }
+        counter = 0;
+      } else {
+        if (counter == 0) {
+          if (!gap) {
+            candyPosition.push(i-1);
+          } else {
+            extraCandyPosition.push(i-1);
+          }
+        }
+        if (!gap) {
+          candyPosition.push(i);
+        } else {
+          extraCandyPosition.push(i);
+        }
+        counter += 1;
+      }
+      comparisonValue = candyRow[i];
+    }
+    if (extraCandyPosition.length > 2) {
+      candyPosition = $.merge(candyPosition, extraCandyPosition);
+    }
+    if (candyPosition.length <= 2) {
+      candyPosition = [];
+    }
+    candyCount = candyPosition.length;
+    if (candyCount >= 3) {
+      deleteHorizontal(candyPosition, candyRow);
+      setScore(candyCount);
+    }
+  }
+}
+
+//Para borrar imagenes
+function deleteHorizontal(candyPosition, candyRow) {
+  for (var i = 0; i < candyPosition.length; i++) {
+    candyRow[candyPosition[i]].addClass('delete');
+  }
+}
+
+//Para la Puntuación
+function setScore(candyCount) {
+  var score = Number($('#score-text').text());
+  switch (candyCount) {
+    case 3:
+      score += 25;
+      break;
+    case 4:
+      score += 50;
+      break;
+    case 5:
+      score += 75;
+      break;
+    case 6:
+      score += 100;
+      break;
+    case 7:
+      score += 200;
+  }
+  $('#score-text').text(score);
+}
+
+//Para llenar el cuadro con las imagenes
+function llenarTablero() {
+  var top = 5 ;
+  var column = $('[class^="col-"]');
+
+  column.each(function() {
+    var candys = $(this).children().length;
+    var agrega = top - candys;
+    for (var i = 0; i < agrega; i++) {
+      //Para agregar imagen al azar
+      var candyType = getRandomInt(1, 5);
+      //Para que si el espacio esta vacio se llene
+      if (i == 0 && candys < 1) {
+        $(this).append('<img src="image/'+candyType+'.png" class="element"></img>');
+      } else {
+        //Para que caigan las imagenes al espacio vacio
+        $(this).find('img:eq(0)').before('<img src="image/'+candyType+'.png" class="element"></img>');
+      }
+    }
+  });
+  addCandyEvents();
+  setValidations();
+}
+
+//Para Validar
+function setValidations() {
+  columnValidation();
+  rowValidation();
+
+  if ($('img.delete').length != 0) {
+    deletesCandyAnimation();
+  }
+}
+
+//Para mover las imagenes
+function addCandyEvents() {
+  $('img').draggable({
+    containment: '.panel-tablero',
+    droppable: 'img',
+    revert: true,
+    revertDuration: 500,
+    grid: [100, 100],
+    zIndex: 10,
+    drag: constrainCandyMovement
+  });
+  $('img').droppable({
+    drop: swapCandy
+  });
+  enableCandyEvents();
+}
+
+function disableCandyEvents() {
+  $('img').draggable('disable');
+  $('img').droppable('disable');
+}
+
+function enableCandyEvents() {
+  $('img').draggable('enable');
+  $('img').droppable('enable');
+}
+
+//Para que una sola imagen no se mueva
+function constrainCandyMovement(event, candyDrag) {
+  candyDrag.position.top = Math.min(100, candyDrag.position.top);
+  candyDrag.position.bottom = Math.min(100, candyDrag.position.bottom);
+  candyDrag.position.left = Math.min(100, candyDrag.position.left);
+  candyDrag.position.right = Math.min(100, candyDrag.position.right);
+}
+
+//Para que cambie una imagen por otra al colocarlo encima
+function swapCandy(event, candyDrag) {
+  var candyDrag = $(candyDrag.draggable);
+  var dragSrc = candyDrag.attr('src');
+  var candyDrop = $(this);
+  var dropSrc = candyDrop.attr('src');
+  candyDrag.attr('src', dropSrc);
+  candyDrop.attr('src', dragSrc);
+
+  setTimeout(function() {
+    llenarTablero();
+    if ($('img.delete').length == 0) {
+      candyDrag.attr('src', dragSrc);
+      candyDrop.attr('src', dropSrc);
+    } else {
+      updateMoves();
+    }
+  }, 500);
+
+}
+
+function llenarTableroPromise(result) {
+  if (result) {
+    llenarTablero();
+  }
+}
+
+//Para calcular la cantidad de movimientos
+function updateMoves() {
+  var actualValue = Number($('#movimientos-text').text());
+  var result = actualValue += 1;
+  $('#movimientos-text').text(result);
+}
+
+//Para titileo al borrar las imagenes
+function deletesCandyAnimation() {
+  disableCandyEvents();
+  $('img.delete').effect('pulsate', 500);
+  $('img.delete').animate({
+    opacity: '0'
+  },{
+    duration: 500
+  }
+  )
+  .animate({
+    opacity: '0'
+  }
+  ,
+  {
+    duration: 1000,
+    complete: function() {
+      deletesCandy()
+        .then(llenarTableroPromise)
+        .catch(showPromiseError)
+    },
+    queue: true
+  }
+  )
+}
+
+function showPromiseError(error) {
+  console.log(error);
+}
+
+
+function deletesCandy() {
+  return new Promise(function (resolve, reject) {
+    if($('img.delete').remove()) {
+      resolve(true);
+    } else {
+      reject('Candy could not be deleted...');
+    }
+  })
+}
+
+//Para el final del juego
+function finJuego() {
+  $('div.panel-tablero, div.time').effect('fold');
+  $('h1.main-titulo').addClass('title-over')
+  .text('Gracias por Jugar!');
+  $('div.score, div.moves, div.panel-score').width('100%');
+}
+
+
+//Para el Boton de Inicio
+  $('.btn-reinicio').click(function() {
+
+    //Para que reinicie el juego al presionar dos veces
+    if ($(this).text() == 'Reiniciar') {
+      location.reload(true);
+    }
+    //Para Iniciar el juego
+    llenarTablero();
+    $(this).text('Reiniciar');
+
+    //Para iniciar los efectos en el titulo
+    coloresTitulo('.main-titulo');
+
+    //Para iniciar el contador
+        $('#timer').startTimer({
+      onComplete: finJuego
+    })
+  });
